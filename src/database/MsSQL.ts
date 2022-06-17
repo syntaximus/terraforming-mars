9022b084ed13e6110e02c17e36d5516257197a6b
Maciej Jakubiak <maciej626@gmail.com>
2022-06-17 02:12:38 +0200

    dostosowanie mssql do wersji 2022-06-15

--- src/database/MsSQL.ts
+++ src/database/MsSQL.ts
@@ -1,12 +1,19 @@
 import { DbLoadCallback, IDatabase } from './IDatabase';
 import { Game, GameOptions, Score } from '../Game';
 import { GameId } from '../common/Types';
-import { IGameData } from '../common/game/IGameData';
 import { SerializedGame } from '../SerializedGame';
 import { ConnectionPool, config } from 'mssql';
 
 export class MsSQL implements IDatabase {
     private client: ConnectionPool;
+    private databaseName: string | undefined = 'terraforming'; // Use this only for stats.
+
+    protected statistics = {
+        saveCount: 0,
+        saveErrorCount: 0,
+        saveConflictUndoCount: 0,
+        saveConflictNormalCount: 0,
+    };
 
     constructor() {
         const config: config = {
@@ -30,6 +37,7 @@ export class MsSQL implements IDatabase {
             };
         });
 
+        
         this.client.connect().then(() => {
             this.client.query(`
                 IF OBJECT_ID(\'dbo.games\', \'U\') IS NULL
@@ -103,89 +111,48 @@ export class MsSQL implements IDatabase {
 
     }
 
-    getClonableGames(cb: (err: Error | undefined, allGames: Array<IGameData>) => void) {
-        const allGames: Array<IGameData> = [];
-        const sql = 'SELECT distinct game_id game_id, players players FROM games WHERE save_id = 0 order by game_id asc';
+    getPlayerCount(game_id: GameId): Promise<number> {
+        const sql = 'SELECT TOP 1 players FROM games WHERE save_id = 0 AND game_id = @game_id';
 
-        this.client.query<any>(sql, (err, res) => {
-            if (err) {
-                console.error('MsSQL:getClonableGames', err);
-                cb(err, []);
-                return;
-            }
-            for (const row of res?.recordsets[0] ?? []) {
-
-                const gameId: GameId = row?.game_id;
-                const playerCount: number = row.players;
-                const gameData: IGameData = {
-                    gameId,
-                    playerCount,
-                };
-                allGames.push(gameData);
-            }
-            cb(undefined, allGames);
-        });
-    }
-
-    getClonableGameByGameId(game_id: GameId, cb: (err: Error | undefined, gameData: IGameData | undefined) => void) {
-        this.client
+        return this.client
             .request()
             .input('game_id', game_id)
-            .query<any>('SELECT TOP 1 players FROM games WHERE save_id = 0 AND game_id = @game_id', (err, res) => {
-            if (err) {
-                console.error('MsSQL:getClonableGameByGameId', err);
-                cb(err, undefined);
-                return;
-            }
-                if (res?.recordsets[0].length === 0) {
-                cb(undefined, undefined);
-                return;
-            }
-            cb(undefined, {
-                gameId: res?.recordsets[0][0].game_id,
-                playerCount: res?.recordsets[0][0].players,
+            .query<any>(sql)
+            .then((res) => {
+                if (res?.recordsets[0].length) {
+                    throw new Error(`no rows found for game id ${game_id}`);
+                }
+                return res?.recordsets[0][0].players;
             });
-        });
     }
 
-    getGames(cb: (err: Error | undefined, allGames: Array<GameId>) => void) {
-        const allGames: Array<GameId> = [];
-        const sql: string = 'SELECT games.game_id FROM games, (SELECT max(save_id) save_id, game_id FROM games GROUP BY game_id) a WHERE games.game_id = a.game_id AND games.save_id = a.save_id ORDER BY created_time DESC';
-        this.client.query<any>(sql, (err, res) => {
-            if (err) {
+    getGames(): Promise<Array<GameId>> {
+        const sql: string = 'SELECT distinct game_id FROM games';
+        return this.client
+            .request()
+            .query<any>(sql)
+            .then((res) => {
+                return res?.recordsets[0].map((row) => row.game_id);
+            })
+            .catch((err) => {
                 console.error('MsSQL:getGames', err);
-                cb(err, []);
-                return;
-            }
-            for (const row of res?.recordsets[0] ?? []) {
-                allGames.push(row.game_id);
-            }
-            cb(undefined, allGames);
-        });
+                throw err;
+            });
     }
 
-    loadCloneableGame(game_id: GameId, cb: DbLoadCallback<SerializedGame>) {
+    loadCloneableGame(game_id: GameId): Promise<SerializedGame> {
         // Retrieve first save from database
-        this.client
+        return this.client
             .request()
             .input('game_id', game_id)
-            .query<any>('SELECT game_id game_id, game game FROM games WHERE game_id = @game_id AND save_id = 0', (err: Error | undefined, res) => {
-                if (err) {
-                    console.error('MsSQL:restoreReferenceGame', err);
-                    return cb(err, undefined);
-                }
+            .query<any>('SELECT game_id game_id, game game FROM games WHERE game_id = @game_id AND save_id = 0')
+            .then((res) => {
                 if (res?.recordsets[0].length === 0) {
-                    return cb(new Error(`Game ${game_id} not found`), undefined);
+                    throw new Error(`Game ${game_id} not found`);
                 }
-                try {
-                    const json = JSON.parse(res?.recordsets[0][0].game);
-                    return cb(undefined, json);
-                } catch (exception) {
-                    const error = exception instanceof Error ? exception : new Error(String(exception));
-                    console.error(`Unable to restore game ${game_id}`, error);
-                    cb(error, undefined);
-                    return;
-                }
+
+                const json = JSON.parse(res?.recordsets[0][0].game);
+                return json;
             });
     }
 
@@ -205,23 +172,69 @@ export class MsSQL implements IDatabase {
                 cb(undefined, JSON.parse(res?.recordsets[0][0].game));
             });
     }
+    
+    getGameId(id: string): Promise<GameId> {
+        let sql = undefined;
+        if (id.charAt(0) === 'p') {
+            sql =
+                `   SELECT game_id
+                    FROM games
+                    OUTER APPLY OPENJSON(game) WITH ( players NVARCHAR(MAX) '$.players' AS JSON) AS j1
+                    OUTER APPLY OPENJSON(j1.players) WITH (player NVARCHAR(max) '$.id') AS j2
+                    WHERE save_id = 0 AND j2.player = @id`;
+        } else if (id.charAt(0) === 's') {
+            sql =
+                `   SELECT game_id
+                    FROM games
+                    OUTER APPLY OPENJSON(game) WITH (spectator NVARCHAR(MAX) '$.spectatorId') AS j1
+                    WHERE save_id = 0 AND j1.spectator = @id`;
+        } else {
+            throw new Error(`id ${id} is neither a player id or spectator id`);
+        }
 
-    // TODO(kberg): throw an error if two game ids exist.
-    getGameId(_playerId: string, _cb: (err: Error | undefined, gameId?: GameId) => void): void {
-        throw new Error('Not implemented');
+        return this.client
+            .request()
+            .input('id', id)
+            .query<any>(sql)
+            .then((res) => {
+                if (res?.recordsets[0].length === 0) {
+                    throw new Error(`Game for player id ${id} not found`);
+                }
+                return res?.recordsets[0][0].game_id;
+            })
+            .catch((err) => {
+                if (err) {
+                    console.error('MsSQL:getGameId', err);
+                    throw err;
+                }
+            });
     }
 
-    getGameVersion(game_id: GameId, save_id: number, cb: DbLoadCallback<SerializedGame>): void {
-        this.client
+    public async getSaveIds(gameId: GameId): Promise<Array<number>> {
+        const res = await this.client
+            .request()
+            .input('game_id', gameId)
+            .query<any>('SELECT distinct save_id FROM games WHERE game_id = @game_id')
+
+        const allSaveIds: Array<number> = [];
+        res?.recordsets[0].forEach((row) => {
+            allSaveIds.push(row.save_id);
+        });
+        return Promise.resolve(allSaveIds);
+    }
+
+    getGameVersion(game_id: GameId, save_id: number): Promise<SerializedGame> {
+        return this.client
             .request()
             .input('game_id', game_id)
             .input('save_id', save_id)
-            .query<any>('SELECT game game FROM games WHERE game_id = @game_id and save_id = @save_id', (err: Error | undefined, res) => {
-                if (err) {
-                    console.error('MsSQL:getGameVersion', err);
-                    return cb(err, undefined);
+            .query<any>('SELECT game game FROM games WHERE game_id = @game_id and save_id = @save_id')
+            .then((res) => {
+                if (res?.recordsets[0].length === 0) {
+                    throw new Error(`Game ${game_id} not found at save_id ${save_id}`);
                 }
-                cb(undefined, JSON.parse(res?.recordsets[0][0].game));
+
+                return JSON.parse(res?.recordsets[0][0].game);
             });
     }
 
@@ -286,8 +299,8 @@ export class MsSQL implements IDatabase {
     }
 
     // Purge unfinished games older than MAX_GAME_DAYS days. If this environment variable is absent, it uses the default of 10 days.
-    purgeUnfinishedGames(): void {
-        const envDays = parseInt(process.env.MAX_GAME_DAYS || '');
+    purgeUnfinishedGames(maxGameDays: string | undefined = process.env.MAX_GAME_DAYS): void {
+        const envDays = parseInt(maxGameDays || '');
         const days = Number.isInteger(envDays) ? envDays : 9999;
         this.client
             .request()
@@ -302,8 +315,9 @@ export class MsSQL implements IDatabase {
             });
     }
 
-    restoreGame(game_id: GameId, save_id: number, cb: DbLoadCallback<Game>): void {
+    restoreGame(game_id: GameId, save_id: number, cb: DbLoadCallback<SerializedGame>): void {
         // Retrieve last save from database
+        logForUndo(game_id, 'restore to', save_id);
         this.client
             .request()
             .input('game_id', game_id)
@@ -321,8 +335,8 @@ export class MsSQL implements IDatabase {
                 }
                 try {
                     // Transform string to json
-                    const json = JSON.parse(res?.recordsets[0][0].game);
-                    const game = Game.deserialize(json);
+                    const game = JSON.parse(res?.recordsets[0][0].game);
+                    logForUndo(game.id, 'restored to', game.lastSaveId, 'from', save_id);
                     cb(undefined, game);
                 } catch (e) {
                     const error = e instanceof Error ? e : new Error(String(e));
@@ -333,7 +347,9 @@ export class MsSQL implements IDatabase {
 
     saveGame(game: Game): Promise<void> {
         const gameJSON = game.toJSON();
-        this.client
+        this.statistics.saveCount++;
+        if (game.gameOptions.undoOption) logForUndo(game.id, 'start save', game.lastSaveId);
+        return this.client
             .request()
             .input('game_id', game.id)
             .input('save_id', game.lastSaveId)
@@ -354,40 +370,124 @@ export class MsSQL implements IDatabase {
                       VALUES(source.game_id, source.save_id, source.game, source.players)
                     WHEN MATCHED THEN
                       UPDATE SET
-                        g.game = source.game;`,
-                (err) => {
-                    if (err) {
-                        console.error('MsSQL:saveGame', err);
-                        return;
+                        g.game = source.game
+                    OUTPUT inserted.*;`)
+            .then((res) => {
+                let inserted: boolean = true;
+                try {
+                    inserted = res?.recordsets[0].length > 0;
+                } catch (err) {
+                    console.error(err);
+                }
+                if (inserted === false) {
+                    if (game.gameOptions.undoOption) {
+                        this.statistics.saveConflictUndoCount++;
+                    } else {
+                        this.statistics.saveConflictNormalCount++;
                     }
-                },
-            );
+                }
 
-        // This must occur after the save.
-        game.lastSaveId++;
-        return Promise.resolve();
+                game.lastSaveId++;
+                if (game.gameOptions.undoOption) logForUndo(game.id, 'increment save id, now', game.lastSaveId);
+            })
+            .catch((err) => {
+                if (err) {
+                    console.error('MsSQL:saveGame', err);
+                    return;
+                }
+            });
+
     }
 
     deleteGameNbrSaves(game_id: GameId, rollbackCount: number): void {
-        if (rollbackCount > 0) {
-            this.client
-                .request()
-                .input('game_id', game_id)
-                .input('rollbackCount', rollbackCount)
-                .query<any>(`
-                    ;WITH CTE AS
-                    (
-                      SELECT TOP (@rollbackCount) *
-                      FROM games
-                      WHERE game_id = @game_id
-                      ORDER BY save_id DESC
-                    )
-                    DELETE FROM CTE`,
-                    (err) => {
-                        if (err) {
-                            return console.warn(err.message);
-                        }
-                    });
+        if (rollbackCount <= 0) {
+            console.error(`invalid rollback count for ${game_id}: $rollbackCount`);
+            return;
         }
+        logForUndo(game_id, 'deleting', rollbackCount, 'saves');
+
+        this
+            .getSaveIds(game_id)
+            .then((first) => {
+                this.client
+                    .request()
+                    .input('game_id', game_id)
+                    .input('rollbackCount', rollbackCount)
+                    .query<any>(`
+                        ;WITH CTE AS
+                        (
+                          SELECT TOP (@rollbackCount) *
+                          FROM games
+                          WHERE game_id = @game_id
+                          ORDER BY save_id DESC
+                        )
+                        DELETE FROM CTE`,
+                        (err, res) => {
+                            if (err) {
+                                console.error(err.message);
+                            }
+                            logForUndo(game_id, 'deleted', res?.rowsAffected, 'rows');
+                            this.getSaveIds(game_id)
+                                .then((second) => {
+                                    const difference = first.filter((x) => !second.includes(x));
+                                    logForUndo(game_id, 'second', second);
+                                    logForUndo(game_id, 'Rollback difference', difference);
+                                });
+                        });
+            });
     }
+
+    public async stats(): Promise<{ [key: string]: string | number }> {
+        const map: { [key: string]: string | number } = {
+            'type': 'MSSQL',
+            'pool-total-count': this.client.pool?.numUsed(),
+            'pool-idle-count': this.client.pool?.numFree(),
+            'pool-waiting-count': this.client.pool?.numPendingAcquires(),
+            'save-count': this.statistics.saveCount,
+            'save-error-count': this.statistics.saveErrorCount,
+            'save-confict-normal-count': this.statistics.saveConflictNormalCount,
+            'save-confict-undo-count': this.statistics.saveConflictUndoCount,
+        };
+
+        // TODO(kberg): return row counts
+        return this.client
+            .request()
+            .input('db_name', this.databaseName)
+            .query<any>(
+                `
+                    SELECT
+                      s.Name AS [schema_name],
+                      t.Name AS [table_name],
+                      p.rows AS [row_counts],
+                      1024 * 1024 * CAST(ROUND((SUM(a.used_pages) / 128.00), 2) AS NUMERIC(36, 2)) AS [used_bytes],
+                      1024 * 1024 * CAST(ROUND((SUM(a.total_pages) - SUM(a.used_pages)) / 128.00, 2) AS NUMERIC(36, 2)) AS [unused_bytes],
+                      1024 * 1024 * CAST(ROUND((SUM(a.total_pages) / 128.00), 2) AS NUMERIC(36, 2)) AS [total_bytes]
+                    FROM sys.tables t
+                      INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
+                      INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+                      INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+                      INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+                    GROUP BY t.Name, s.Name, p.Rows
+                    ORDER BY s.Name, t.Name;
+
+                    SELECT
+                      DB_NAME(database_id) AS [database_name],
+                      1024 * SUM(CASE WHEN type_desc = 'LOG' THEN size END) * 8 AS [log_size_bytes],
+                      1024 * SUM(CASE WHEN type_desc = 'ROWS' THEN size END) * 8 AS [row_size_bytes],
+                      1024 * SUM(size) * 8 AS [total_size_bytes]
+                    FROM sys.master_files WITH(NOWAIT)
+                    WHERE database_id = DB_ID('terraforming') -- for current db
+                    GROUP BY database_id
+                `)
+            .then((res) => {
+                map['size-bytes-games'] = res?.recordsets[0][0].used_bytes;
+                map['size-bytes-game-results'] = res?.recordsets[0][1].used_bytes;
+                map['size-bytes-database'] = res?.recordsets[1][0].total_size_bytes;
+                return map;
+            });
+    }
+}
+
+function logForUndo(gameId: string, ...message: any[]) {
+    console.error(['TRACKING:', gameId, ...message]);
 }
